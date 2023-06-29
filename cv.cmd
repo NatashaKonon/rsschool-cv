ead9558c33c3b196bebf2a0d5afffb588c8e8c82
NatashaKonon <natashakonon776@gmail.com>
2023-06-29 17:05:29 +0300

    fix: exchange cv.cmd

---
+++ cv.cmd
@@ -0,0 +1,30 @@
+#Natalsha Konon
+Junior Java-JavaScript Developer, Freelancer
+![image](https://github.com/settings/profile)
+
+##Contacts
+- telephone: +375256539299
+- discord: @natashakonon
+- natashakonon776@gmail.com
+
+##My skills:
+- Git 
+- HTML, CSS
+- basic level of JavaScript
+
+##My best qualities
+perseverance, self-organization, industriousness
+
+##Education
+-Belarusion state university of culture and art, Faculty of information and document communication
+-Institute of information technologies BSUIR, Faculty of information systems software
+-University of National Academy of science, Magistracy
+-RS School
+
+##Languages
+Belarussion - native
+Russion - perfect
+English - b1
+
+
+
